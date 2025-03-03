7c86420a3814cad370460ebeb49f20f34eb27e93
Oskar Emilsson <oskar.emilsson@bontouch.com>
2025-03-03 11:25:03 +0100

    Initial Commit

---
+++ ProductQuery/ProductQueryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductQueryApp.swift
+//  ProductQuery
+//
+//  Created by Oskar Emilsson on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct ProductQueryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductQueryTests/ProductQueryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductQueryTests.swift
+//  ProductQueryTests
+//
+//  Created by Oskar Emilsson on 2025-03-03.
+//
+
+import Testing
+@testable import ProductQuery
+
+struct ProductQueryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
